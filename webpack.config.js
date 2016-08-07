397a9ca8431db990cebdb1e894f522db8906d570
Aksana_Tolstoguzova <Aksana_Tolstoguzova@epam.com>
2016-08-07 13:36:12 +0300

    Added eslint, format code, add rules for global object

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,60 @@
-'use strict'
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
-
-let path = require('path');
-let autoprefixer = require('autoprefixer');
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
-    context: __dirname + '/src',
-    entry: {
-        app: ['es6-promise', 'whatwg-fetch', './js/app']
-    },
-    output: {
-        path: __dirname + '/build/js',
-        filename: '[name].js',
-        library: '[name]'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel',
-                test: /\.js$/,
-                exclude: /node_modules/,
-                query: {
-                    plugins: ['transform-runtime'],
-                    presets: ['es2015']
-                },
-            },
-            {
-                test: /\.css$/,
-                loader: 'style!css-loader!postcss-loader'
-            },
-            {
-              test: /\.scss$/,
-              loader: 'style!css-loader!postcss-loader!sass-loader'
-            }
-        ]
-    },
-    postcss: [ autoprefixer({ browsers: ['last 3 versions'] }) ],
-    
-    devtool: NODE_ENV == 'development' ? "cheap-module-source-map" : null,
-
-    plugins: NODE_ENV == 'development' ?[ 
-        new webpack.NoErrorsPlugin()
-    ] : [
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
-    })
+  context: __dirname + '/src',
+  entry: {
+    app: ['es6-promise', 'whatwg-fetch', './js/app'],
+  },
+  output: {
+    path: __dirname + '/build/js',
+    filename: '[name].js',
+    library: '[name]',
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+          plugins: ['transform-runtime'],
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader!postcss-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style!css-loader!postcss-loader!sass-loader',
+      },
     ],
-    devServer: {
-    },
+  },
+  postcss: [autoprefixer({ browsers: ['last 3 versions'] })],
 
-    resolve: {
+  devtool: NODE_ENV === 'development' ? 'cheap-module-source-map' : null,
+
+  plugins: NODE_ENV === 'development' ? [
+    new webpack.NoErrorsPlugin(),
+  ] : [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+    }),
+  ],
+  devServer: {},
+
+  resolve: {
     root: [
-        path.resolve('./src/js'),
-        path.resolve('./src/scss'),
+      path.resolve('./src/js'),
+      path.resolve('./src/scss'),
     ],
-    extensions: ['', '.js', '.scss']
-  }
+    extensions: ['', '.js', '.scss'],
+  },
 
 };
